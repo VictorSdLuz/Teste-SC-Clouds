01ba2973c4c27b1d217961843d0438760414598d
Victor Santos da Luz <103959610+VictorSdLuz@users.noreply.github.com>
2023-01-16 21:06:47 -0300

    Update FibonnaciRecursiva.java

--- src/questoes/FibonnaciRecursiva.java
+++ src/questoes/FibonnaciRecursiva.java
@@ -1,3 +1,9 @@
+//1 - Fibonnaci
+//    -- Criar uma função em sua linguagem preferida. A função deve receber um numero N >= 0 (deve validar o input para a função), e retornar o valor correspondente 
+//      desse número na sequencia fibonnaci. EX. fib(0) =0; fib(1) = 1; fib(2) = 1; fib(3) = 2; fib(5) = 5; fib(6) = 8.
+//    --- Criar uma função recursiva que resolva fibonacci
+//    --- Criar uma função linear que resolva fibonnaci
+
 package questoes;
 
 import java.util.Scanner;
